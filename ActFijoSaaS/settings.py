e0fa707085c2ee421436df2f8aeb73be46792de7
Guillermo VR <guillermo.vaca07@gmail.com>
2025-11-07 10:12:36 -0400

    ignore

--- ActFijoSaaS/settings.py
+++ ActFijoSaaS/settings.py
@@ -50,8 +50,20 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'ActFijoSaaS.urls'
 WSGI_APPLICATION = 'ActFijoSaaS.wsgi.application'
-TEMPLATES = [ 
-    # ... (tu configuración de templates está bien)
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
 
 
